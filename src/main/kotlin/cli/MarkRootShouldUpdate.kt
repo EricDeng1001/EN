a13d0263bb16594e59bb185f17109dadaaa93b2f
ericdeng <gantinus@gmail.com>
2024-01-07 13:49:22 +0800

    add script to set all root to should update

--- src/main/kotlin/cli/MarkRootShouldUpdate.kt
+++ src/main/kotlin/cli/MarkRootShouldUpdate.kt
@@ -6,9 +6,7 @@ import kotlinx.coroutines.runBlocking
 fun main(args: Array<String>) {
     runBlocking {
         val queryAllRoot = MongoNodeRepository.queryAllRoot()
-        for (root in queryAllRoot) {
-            root.shouldUpdate = true
-        }
+        queryAllRoot.forEach { it.shouldUpdate = true }
         MongoNodeRepository.saveAll(queryAllRoot)
     }
 }
