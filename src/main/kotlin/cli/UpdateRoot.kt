d44811e39af379166063247c54bb4f04ab840780
liyi <294206591@qq.com>
2024-01-12 01:18:38 +0800

    cli update

--- src/main/kotlin/cli/UpdateRoot.kt
+++ src/main/kotlin/cli/UpdateRoot.kt
@@ -6,11 +6,12 @@ import model.DataId
 
 suspend fun main() {
     val x = listOf(
-        "low_min", "high_min", "money_min", " volume_min", "close_min", "gz2000_close_min", "cj_td_min",
+        "low_min", "high_min", "money_min", "volume_min", "close_min", "gz2000_close_min", "cj_td_min",
         "open_min", "circulating_market_cap", "adj_factor", "close", "sector", "low", "high", "turnover_ratio",
         "vwap_min", "cd_lo_min", "cd_hi_min"
     ).map { DataId(it) }.toList()
+
     ExpressionNetworkImpl.forceUpdateRoot(x)
 
-    delay(3600 * 1000)
+    delay(4800 * 1000)
 }
